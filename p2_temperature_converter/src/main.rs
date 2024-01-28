013c9df27064d51bee1c94c8a08ea163b62b5314
Cody Ratcliffe <codyratcliffep@gmail.com>
2024-01-28 11:22:32 -0700

    added to user options

--- p2_temperature_converter/src/main.rs
+++ p2_temperature_converter/src/main.rs
@@ -15,6 +15,7 @@
 
 fn main() {
     print_instructions();
+    user_option_select();
     user_input();
     calculate_conversion();
     print_solution();
@@ -25,6 +26,34 @@ fn print_instructions() {
         "Convert temperatures\n\
         between Celsius and Fahrenheit"
     )
+}
+fn user_option_select() -> bool {
+    loop {
+        let mut user_option = String::new();
+        println!(
+            "Select between:\n\
+            1. Celsius to Fahrenheit\n\
+            2. Fahrenheit to Celsius"
+        );
+        std::io::stdin()
+            .read_line(&mut user_option)
+            .expect("Failed to read line");
+
+        match user_option.trim() {
+            "1" => {
+                println!("Celsius to Fahrenheit");
+                return true;
+            }
+            "2" => {
+                println!("Fahrenheit to Celsius");
+                return false;
+            }
+            _ => {
+                println!("Invalid choice.");
+                continue;
+            }
+        }
+    }
 }
 fn user_input() {}
 fn calculate_conversion() {}
